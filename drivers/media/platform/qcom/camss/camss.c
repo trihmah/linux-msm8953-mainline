2cd05f13b4b20212efb61ee459ce0ac08eee2c1c
Vladimir Lypak <vladimir.lypak@gmail.com>
2025-10-24 16:34:37 +0000

    media: qcom: camss: add resource for sdm450
    SDM450 can use CSID2 only for test patterns.
    
    Signed-off-by: Vladimir Lypak <vladimir.lypak@gmail.com>
    [Remove link_entities]
    Signed-off-by: Barnabás Czémán <barnabas.czeman@mainlining.org>

--- drivers/media/platform/qcom/camss/camss.c
+++ drivers/media/platform/qcom/camss/camss.c
@@ -4275,6 +4275,19 @@ static const struct camss_resources msm8916_resources = {
 	.vfe_num = ARRAY_SIZE(vfe_res_8x16),
 };
 
+static const struct camss_resources sdm450_resources = {
+	.version = CAMSS_8x53,
+	.icc_res = icc_res_8x53,
+	.icc_path_num = ARRAY_SIZE(icc_res_8x53),
+	.csiphy_res = csiphy_res_8x96,
+	.csid_res = csid_res_8x53,
+	.ispif_res = &ispif_res_8x53,
+	.vfe_res = vfe_res_8x53,
+	.csiphy_num = ARRAY_SIZE(csiphy_res_8x96),
+	.csid_num = MIN(2, ARRAY_SIZE(csid_res_8x53)),
+	.vfe_num = ARRAY_SIZE(vfe_res_8x53),
+};
+
 static const struct camss_resources msm8953_resources = {
 	.version = CAMSS_8x53,
 	.icc_res = icc_res_8x53,
@@ -4442,6 +4455,7 @@ static const struct camss_resources x1e80100_resources = {
 static const struct of_device_id camss_dt_match[] = {
 	{ .compatible = "qcom,msm8916-camss", .data = &msm8916_resources },
 	{ .compatible = "qcom,msm8953-camss", .data = &msm8953_resources },
+	{ .compatible = "qcom,sdm450-camss", .data = &sdm450_resources },
 	{ .compatible = "qcom,msm8996-camss", .data = &msm8996_resources },
 	{ .compatible = "qcom,qcm2290-camss", .data = &qcm2290_resources },
 	{ .compatible = "qcom,qcs8300-camss", .data = &qcs8300_resources },
