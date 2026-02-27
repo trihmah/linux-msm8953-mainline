77bcb57da28e3da62d45167bfa948a76ed5dc3ab
Vladimir Lypak <vladimir.lypak@gmail.com>
2026-02-27 14:21:58 +0000

    fixup! clk: qcom: clk-alpha-pll: add VCO check/clamping in huayra ops

--- drivers/clk/qcom/clk-alpha-pll.c
+++ drivers/clk/qcom/clk-alpha-pll.c
@@ -1015,11 +1015,6 @@ static int alpha_pll_huayra_set_rate(struct clk_hw *hw, unsigned long rate,
 	u32 l, a, ctl, cur_alpha = 0;
 
 	rate = alpha_huayra_pll_round_rate(rate, prate, &l, &a);
-	if (pll->vco_table && !alpha_pll_find_vco(pll, rate)) {
-		pr_err("%s: rate is not in a valid vco range: %lu\n",
-		       clk_hw_get_name(hw), rate);
-		return -EINVAL;
-	}
 
 	regmap_read(pll->clkr.regmap, PLL_USER_CTL(pll), &ctl);
 
@@ -1060,23 +1055,18 @@ static int alpha_pll_huayra_determine_rate(struct clk_hw *hw,
 					   struct clk_rate_request *req)
 {
 	struct clk_alpha_pll *pll = to_clk_alpha_pll(hw);
-	unsigned long min_freq, max_freq, ret;
+	unsigned long min_freq, max_freq;
 	u32 l, a;
 
-	ret = alpha_huayra_pll_round_rate(req->rate,
-					  req->best_parent_rate, &l, &a);
+	if (pll->vco_table && !alpha_pll_find_vco(pll, req->rate)) {
+		min_freq = pll->vco_table[0].min_freq;
+		max_freq = pll->vco_table[pll->num_vco - 1].max_freq;
 
-
-	if (!pll->vco_table || alpha_pll_find_vco(pll, ret)) {
-		req->rate = ret;
-
-		return 0;
+		req->rate = clamp(req->rate, min_freq, max_freq);
 	}
 
-	min_freq = pll->vco_table[0].min_freq;
-	max_freq = pll->vco_table[pll->num_vco - 1].max_freq;
-
-	req->rate = clamp(ret, min_freq, max_freq);
+	req->rate = alpha_huayra_pll_round_rate(req->rate,
+						req->best_parent_rate, &l, &a);
 
 	return 0;
 }
